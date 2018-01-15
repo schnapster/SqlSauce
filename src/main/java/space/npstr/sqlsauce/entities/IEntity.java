1aa5cf9ba4df3ae56082813812cf1e09aa87b633
Napster <napster@npstr.space>
2018-01-15 10:45:00 +0100

    Add missing hibernate types to IEntity

--- src/main/java/space/npstr/sqlsauce/entities/IEntity.java
+++ src/main/java/space/npstr/sqlsauce/entities/IEntity.java
@@ -26,6 +26,12 @@ package space.npstr.sqlsauce.entities;
 
 import com.vladmihalcea.hibernate.type.array.IntArrayType;
 import com.vladmihalcea.hibernate.type.array.StringArrayType;
+import com.vladmihalcea.hibernate.type.basic.NullableCharacterType;
+import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import com.vladmihalcea.hibernate.type.json.JsonNodeBinaryType;
+import com.vladmihalcea.hibernate.type.json.JsonNodeStringType;
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
 import org.hibernate.annotations.TypeDef;
 import org.hibernate.annotations.TypeDefs;
 import space.npstr.sqlsauce.hibernate.types.ArrayListLongUserType;
@@ -53,6 +59,30 @@ import java.io.Serializable;
         @TypeDef(
                 name = "int-array",
                 typeClass = IntArrayType.class
+        ),
+        @TypeDef(
+                name = "json-node",
+                typeClass = JsonNodeStringType.class
+        ),
+        @TypeDef(
+                name = "jsonb-node",
+                typeClass = JsonNodeBinaryType.class
+        ),
+        @TypeDef(
+                name = "json",
+                typeClass = JsonStringType.class
+        ),
+        @TypeDef(
+                name = "jsonb",
+                typeClass = JsonBinaryType.class
+        ),
+        @TypeDef(
+                name = "pgsql_enum",
+                typeClass = PostgreSQLEnumType.class
+        ),
+        @TypeDef(
+                name = "nullable-char",
+                typeClass = NullableCharacterType.class
         ),
         @TypeDef(
                 name = "long-array",
