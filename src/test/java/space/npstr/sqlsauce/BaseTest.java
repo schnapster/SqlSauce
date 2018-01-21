52928a80cd2220c45bfd67d51087d1b834bc5316
Napster <napster@npstr.space>
2018-01-21 11:44:01 +0100

    Add testing

---
+++ src/test/java/space/npstr/sqlsauce/BaseTest.java
@@ -0,0 +1,78 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017-2018, Dennis Neufeld
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package space.npstr.sqlsauce;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Created by napster on 20.01.18.
+ * <p>
+ * Provides a database connection
+ */
+@ExtendWith(LogExceptionExtension.class)
+public abstract class BaseTest {
+
+    public static final String TEST_JDBC_URL_ENV = "TEST_DB_JDBC";
+
+    private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
+    private static DatabaseConnection dbConn;
+
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            if (dbConn != null) {
+                dbConn.shutdown();
+            }
+        }));
+    }
+
+    @Nonnull
+    protected static String getTestJdbcUrl() {
+        String jdbc = System.getenv(TEST_JDBC_URL_ENV);
+        Assertions.assertNotNull(jdbc, String.format("Jdbc test url %s environment variable is null", TEST_JDBC_URL_ENV));
+        return jdbc;
+    }
+
+    @Nonnull
+    private synchronized static DatabaseConnection getDbConn() {
+        if (dbConn == null) {
+            dbConn = new DatabaseConnection.Builder(BaseTest.class.getSimpleName(), getTestJdbcUrl())
+                    .setHibernateProperty("hibernate.hbm2ddl.auto", "none")
+                    .build();
+        }
+        return dbConn;
+    }
+
+    @Nonnull
+    public DatabaseConnection requireConnection() {
+        DatabaseConnection conn = getDbConn();
+        Assertions.assertTrue(conn.isAvailable(), "Database connection is unavailable. Is the test database up and running?");
+        return conn;
+    }
+}
---
+++ src/test/java/space/npstr/sqlsauce/databasewrapper/NotifyTest.java
@@ -0,0 +1,69 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017-2018, Dennis Neufeld
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package space.npstr.sqlsauce.databasewrapper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import space.npstr.sqlsauce.BaseTest;
+import space.npstr.sqlsauce.DatabaseWrapper;
+import space.npstr.sqlsauce.NotificationService;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by napster on 20.01.18.
+ */
+class NotifyTest extends BaseTest {
+
+    @Test
+    void pgNotify() throws InterruptedException {
+        DatabaseWrapper wrapper = new DatabaseWrapper(requireConnection());
+
+        int interval = 100;
+        String channel = "foo";
+        String payload = "bar";
+
+        AtomicInteger exceptions = new AtomicInteger(0);
+        NotificationService ns = new NotificationService(getTestJdbcUrl(), NotifyTest.class.getSimpleName(),
+                interval, e -> exceptions.incrementAndGet());
+
+        AtomicInteger notifications = new AtomicInteger(0);
+        AtomicBoolean isBar = new AtomicBoolean(false);
+        ns.addListener(notification -> {
+            notifications.getAndIncrement();
+            isBar.set(notification.getParameter().equals(payload));
+        }, channel);
+
+        Thread.sleep(interval * 2);
+        wrapper.notif(channel, payload);
+        Thread.sleep(interval * 2);
+
+        Assertions.assertEquals(0, exceptions.get(), "NotificationService threw exceptions");
+        Assertions.assertEquals(1, notifications.get(), "Did not receive notification");
+        Assertions.assertTrue(isBar.get(), "Payload was wrong");
+        ns.shutdown();
+    }
+}
