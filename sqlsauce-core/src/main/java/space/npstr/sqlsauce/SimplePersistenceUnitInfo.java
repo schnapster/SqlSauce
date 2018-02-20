cbeea37bb0d8d3fb2a5d977688c680f9fbf03635
Napster <napster@npstr.space>
2018-02-20 01:41:37 +0100

    Replace wonky classpath scanner with a proper one

--- sqlsauce-core/src/main/java/space/npstr/sqlsauce/SimplePersistenceUnitInfo.java
+++ sqlsauce-core/src/main/java/space/npstr/sqlsauce/SimplePersistenceUnitInfo.java
@@ -24,10 +24,10 @@
 
 package space.npstr.sqlsauce;
 
+import com.google.common.reflect.ClassPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.CheckReturnValue;
 import javax.annotation.Nullable;
 import javax.persistence.SharedCacheMode;
 import javax.persistence.ValidationMode;
@@ -35,19 +35,13 @@ import javax.persistence.spi.ClassTransformer;
 import javax.persistence.spi.PersistenceUnitInfo;
 import javax.persistence.spi.PersistenceUnitTransactionType;
 import javax.sql.DataSource;
-import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Properties;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -124,8 +118,9 @@ public class SimplePersistenceUnitInfo implements PersistenceUnitInfo {
         return entityPackages.stream()
                 .flatMap(entityPackage -> {
                     try {
-                        return getClassesForPackage(entityPackage).stream().map(Class::getName);
-                    } catch (DatabaseException e) {
+                        ClassPath classPath = ClassPath.from(SimplePersistenceUnitInfo.class.getClassLoader());
+                        return classPath.getTopLevelClassesRecursive(entityPackage).stream().map(ClassPath.ClassInfo::toString);
+                    } catch (Exception e) {
                         log.error("Failed to load entity package {}", entityPackage, e);
                         return Stream.empty();
                     }
@@ -176,87 +171,5 @@ public class SimplePersistenceUnitInfo implements PersistenceUnitInfo {
     @Override
     public ClassLoader getNewTempClassLoader() {
         return null;
-    }
-
-
-    //ugly code below this don't look please
-    //https://stackoverflow.com/a/3527428
-    // its only here to avoid the mistake of forgetting to manually add an entity class to the jpa managed classes
-    // why, you ask? because I want to avoid using xml files to configure the database connection (no reason really, I
-    // just want to know if it's possible), but at the same time I don't want to add spring or other frameworks who
-    // allow xml free configuration (and have methods to add whole packages to be monitored for managed classes)
-    @CheckReturnValue
-    private static List<Class<?>> getClassesForPackage(final String pkgName) throws DatabaseException {
-        final List<Class<?>> classes = new ArrayList<>();
-        // Get a File object for the package
-        File directory;
-        final String fullPath;
-        final String relPath = pkgName.replace('.', '/');
-        log.trace("ClassDiscovery: Package: " + pkgName + " becomes Path:" + relPath);
-        final URL resource = ClassLoader.getSystemClassLoader().getResource(relPath);
-        log.trace("ClassDiscovery: Resource = " + resource);
-        if (resource == null) {
-            throw new DatabaseException("No resource for " + relPath);
-        }
-        fullPath = resource.getFile();
-        log.trace("ClassDiscovery: FullPath = " + resource);
-
-        try {
-            directory = new File(resource.toURI());
-        } catch (final URISyntaxException e) {
-            throw new DatabaseException(pkgName + " (" + resource + ") does not appear to be a valid URL / URI.  Strange, since we got it from the system...", e);
-        } catch (final IllegalArgumentException e) {
-            directory = null;
-        }
-        log.trace("ClassDiscovery: Directory = " + directory);
-
-        if (directory != null && directory.exists()) {
-            // Get the list of the files contained in the package
-            final String[] files = directory.list();
-            if (files != null) {
-                for (final String file : files) {
-                    // we are only interested in .class files
-                    if (file.endsWith(".class")) {
-                        // removes the .class extension
-                        final String className = pkgName + '.' + file.substring(0, file.length() - 6);
-                        log.trace("ClassDiscovery: className = " + className);
-                        try {
-                            classes.add(Class.forName(className));
-                        } catch (final ClassNotFoundException e) {
-                            throw new DatabaseException("ClassNotFoundException loading " + className);
-                        }
-                    }
-                }
-            }
-        } else {
-            final String jarPath = fullPath.replaceFirst("[.]jar[!].*", ".jar").replaceFirst("file:", "");
-            try (final JarFile jarFile = new JarFile(jarPath)) {
-                final Enumeration<JarEntry> entries = jarFile.entries();
-                while (entries.hasMoreElements()) {
-                    final JarEntry entry = entries.nextElement();
-                    final String entryName = entry.getName();
-                    if (entryName.startsWith(relPath) && entryName.length() > (relPath.length() + "/".length())) {
-                        log.trace("ClassDiscovery: JarEntry: " + entryName);
-                        final String className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
-
-                        //skip packages
-                        if (className.endsWith(".")) {
-                            continue;
-                        }
-                        //just a class
-                        log.trace("ClassDiscovery: className = " + className);
-                        try {
-                            classes.add(Class.forName(className));
-                        } catch (final ClassNotFoundException e) {
-                            throw new DatabaseException("ClassNotFoundException loading " + className);
-                        }
-
-                    }
-                }
-            } catch (final IOException e) {
-                throw new DatabaseException(pkgName + " (" + directory + ") does not appear to be a valid package", e);
-            }
-        }
-        return classes;
     }
 }
